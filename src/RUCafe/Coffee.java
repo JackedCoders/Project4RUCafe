46091c6c45cc380c23a87e7033046d7b0084a5cb
Prasidh Sriram <prasidh.sw1ft@gmail.com>
2021-03-31 11:06:48 -0400

    coffee prices

--- src/RUCafe/Coffee.java
+++ src/RUCafe/Coffee.java
@@ -5,6 +5,11 @@ package RUCafe;
  */
 public class Coffee extends MenuItem implements Customizable{
 
+    final double SHORT_COFFEE_PRICE = 1.99;
+    final double TALL_COFFEE_PRICE = 2.49;
+    final double GRANDE_COFFEE_PRICE = 2.99;
+    final double VENTI_COFFEE_PRICE = 3.49;
+
     public double itemPrice(){
         return 0;
     }
