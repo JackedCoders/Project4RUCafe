42633fdc91821dad6c173ee67400a59fef6f0851
manveer_0311 <Manveer.singh0311@gmail.com>
2021-03-31 13:27:38 -0400

    update

--- src/RUCafe/Main.java
+++ src/RUCafe/Main.java
@@ -7,17 +7,23 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
- *
+ * Main class for our project, creates the stage and loads the primary GUI of the project (MainMenuView.fxml). The class
+ * also contains the main method, runs the corresponding fxml files along with setting title for our stage and initializes
+ * the scene.
  * @author Manveer Singh, Prasidh Sriram
  */
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("MainMenuView.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("MainMenuView.fxml"));
+            primaryStage.setTitle("Welcome to RU Cafe!!");
+            primaryStage.setScene(new Scene(root, 300, 275));
+            primaryStage.show();
+        } catch( Exception e){
+            e.printStackTrace();
+        }
     }
 
 
