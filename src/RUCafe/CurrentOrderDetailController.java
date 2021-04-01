ae6a4e10153148717846ee1a27dcd3aa61b097c3
manveer_0311 <Manveer.singh0311@gmail.com>
2021-03-31 22:53:22 -0400

    more work u not doing

--- src/RUCafe/CurrentOrderDetailController.java
+++ src/RUCafe/CurrentOrderDetailController.java
@@ -1,8 +1,21 @@
 package RUCafe;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 /**
  *
  * @author Manveer Singh, Prasidh Sriram
  */
 public class CurrentOrderDetailController {
+
+    @FXML
+    private TextArea subTotalCurrentOrder, salesTaxCurrentOrder, totalCurrentOrder;
+
+    @FXML
+    private Button RemoveSelectedItem, placeOrder;
+
+    @FXML
+    private ListView currentOrderListView;
+
 }
--- src/RUCafe/OrderingCoffeeController.java
+++ src/RUCafe/OrderingCoffeeController.java
@@ -1,8 +1,37 @@
 package RUCafe;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  *
  * @author Manveer Singh, Prasidh Sriram
  */
 public class OrderingCoffeeController {
+
+    @FXML
+    private CheckBox Milk, Syrup, Caramel, Cream, WhippedCream;
+
+    @FXML
+    private TextArea textArea;
+
+    @FXML
+    private Button addToOrder;
+
+    @FXML
+    private ComboBox coffeeOrderSize, coffeeOrderQuantity;
+
+
+    public void initialize(URL url, ResourceBundle resourceBundle){
+        ObservableList<String> orderSize = FXCollections.observableArrayList("Short", "Tall", "Grande", "Venti");
+        ObservableList<String> quantitySize = FXCollections.observableArrayList("1", "2", "3", "4", "5");
+        coffeeOrderSize.setItems(orderSize);
+        coffeeOrderQuantity.setItems(quantitySize);
+    }
 }
