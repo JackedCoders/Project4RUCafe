a2b8e388069fe7481cbc1689abd7a67dadb736df
manveer_0311 <Manveer.singh0311@gmail.com>
2021-04-01 02:02:37 -0400

    GUIs fully done...can u do ur part nig

--- src/RUCafe/MainMenuController.java
+++ src/RUCafe/MainMenuController.java
@@ -1,8 +1,15 @@
 package RUCafe;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 /**
  *
@@ -14,4 +21,63 @@ public class MainMenuController {
     private Button OrderDonutsButton, OrderCoffeeButton, ViewStoreOrdersButton, ViewYourOrderButton;
 
 
+    /**
+     *
+     * @param event
+     * @throws IOException
+     */
+    @FXML
+    private void openCoffeeOrdering(ActionEvent event) throws IOException{
+        Parent root_parent = FXMLLoader.load(getClass().getResource("OrderingCoffeeView.fxml"));
+        Scene root_scene = new Scene(root_parent);
+        Stage root_stage = new Stage();
+        root_stage.setScene(root_scene);
+        root_stage.setTitle("Order Coffee here!!");
+        root_stage.show();
+    }
+
+    /**
+     *
+     * @param event
+     * @throws IOException
+     */
+    @FXML
+    private void openDonutOrdering(ActionEvent event) throws IOException{
+        Parent root_parent = FXMLLoader.load(getClass().getResource("OrderingDonutsView.fxml"));
+        Scene root_scene = new Scene(root_parent);
+        Stage root_stage = new Stage();
+        root_stage.setScene(root_scene);
+        root_stage.setTitle("Order Donut here!!");
+        root_stage.show();
+    }
+
+    /**
+     *
+     * @param event
+     * @throws IOException
+     */
+    @FXML
+    private void viewAllStoreOrders(ActionEvent event) throws IOException{
+        Parent root_parent = FXMLLoader.load(getClass().getResource("StoreOrdersView.fxml"));
+        Scene root_scene = new Scene(root_parent);
+        Stage root_stage = new Stage();
+        root_stage.setScene(root_scene);
+        root_stage.setTitle("All of store orders here!!");
+        root_stage.show();
+    }
+
+    /**
+     *
+     * @param event
+     * @throws IOException
+     */
+    @FXML
+    private void viewYourCurrentOrder(ActionEvent event) throws IOException{
+        Parent root_parent = FXMLLoader.load(getClass().getResource("CurrentOrderDetailView.fxml"));
+        Scene root_scene = new Scene(root_parent);
+        Stage root_stage = new Stage();
+        root_stage.setScene(root_scene);
+        root_stage.setTitle("Your current order!!");
+        root_stage.show();
+    }
 }
