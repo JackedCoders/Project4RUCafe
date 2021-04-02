084e08d5ec3eee8722f0d1a3c98446ffdd90c5e4
manveer_0311 <Manveer.singh0311@gmail.com>
2021-04-02 02:05:44 -0400

    donut

--- src/RUCafe/OrderingDonutsController.java
+++ src/RUCafe/OrderingDonutsController.java
@@ -1,19 +1,23 @@
 package RUCafe;
 
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
-
+import javafx.scene.control.ComboBox;
 import javax.print.CancelablePrintJob;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  *
  * @author Manveer Singh, Prasidh Sriram
  */
-public class OrderingDonutsController {
+public class OrderingDonutsController implements Initializable {
 
     @FXML
-    private ListView donutFlavorListView, donutAddedListView;
+    private ListView<String> donutFlavorListView, donutAddedListView;
 
     @FXML
     private Button addToOrder, removeSelectedDonut;
@@ -22,8 +26,20 @@ public class OrderingDonutsController {
     private TextArea subTotalTextArea;
 
     @FXML
-    private ComboBox typeOfDonut, quantityOfDonut;
+    private ComboBox<String> typeOfDonut, quantityOfDonut;
 
+    private int yeastDonut = 0;
+    private int cakeDonut = 1;
+    private int donutHole = 2;
+    Donut newDonut = new Donut(0);
+    Order newOrder = new Order();
+    Alert neAlert = new Alert(Alert.AlertType.WARNING);
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+        ObservableList<String> typeOfDonut = FXCollections.observableArrayList("Yeast Donut" , "Cake Donut" , "Donut Holes");
+        ObservableList<String> quantityOfDonut = FXCollections.observableArrayList("1" , "2", "3" );
+        
+    }
 
 }
