e03bb59b22c4936fd402497f75090e19f48d397a
Prasidh Sriram <prasidh.sw1ft@gmail.com>
2021-04-01 20:36:34 -0400

    cofee

--- src/RUCafe/Coffee.java
+++ src/RUCafe/Coffee.java
@@ -6,17 +6,29 @@ package RUCafe;
  */
 public class Coffee extends MenuItem implements Customizable{
 
+    final double COST_PER_ADDIN = 0.2;
     final double SHORT_COFFEE_PRICE = 1.99;
     final double TALL_COFFEE_PRICE = 2.49;
     final double GRANDE_COFFEE_PRICE = 2.99;
     final double VENTI_COFFEE_PRICE = 3.49;
 
+    private int coffeeSize; //0 == short, 1 == tall, 2 ==grande, 3 == venti
+    private int numAddIns;
+
     /**
      *
      * @return
      */
-    public double itemPrice(){
-        return 0;
+    public void itemPrice(){
+        if(coffeeSize == 0){
+            super.itemPrice(COST_PER_ADDIN*numAddIns + SHORT_COFFEE_PRICE);
+        }if(coffeeSize == 1){
+            super.itemPrice(COST_PER_ADDIN*numAddIns + TALL_COFFEE_PRICE);
+        }if(coffeeSize == 2){
+            super.itemPrice(COST_PER_ADDIN*numAddIns + GRANDE_COFFEE_PRICE);
+        }if(coffeeSize == 3){
+            super.itemPrice(COST_PER_ADDIN*numAddIns + VENTI_COFFEE_PRICE);
+        }
     }
 
     /**
--- src/RUCafe/MenuItem.java
+++ src/RUCafe/MenuItem.java
@@ -5,4 +5,10 @@ package RUCafe;
  * @author Manveer Singh, Prasidh Sriram
  */
 public class MenuItem {
+
+    private double itemPrice;
+
+    public void itemPrice(double itemPrice){
+        this.itemPrice = itemPrice;
+    }
 }
