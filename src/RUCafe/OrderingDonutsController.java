49dac891761706d43c05013e1400f1e24262a77f
manveer_0311 <Manveer.singh0311@gmail.com>
2021-04-02 23:04:11 -0400

    lm

--- src/RUCafe/OrderingDonutsController.java
+++ src/RUCafe/OrderingDonutsController.java
@@ -4,11 +4,14 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.*;
 import javafx.scene.control.ComboBox;
 import javax.print.CancelablePrintJob;
 import java.awt.event.MouseEvent;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -152,6 +155,21 @@ public class OrderingDonutsController implements Initializable {
                 addToOrder.setDisable(false);
             }
         }
+    }
+
+    /**
+     *
+     * @param event
+     */
+    @FXML
+    void addToFinalOrder(MouseEvent event) throws IOException {
+        FXMLLoader sendOrder = new FXMLLoader(getClass().getResource("CurrentOrderDetailView.fxml"));
+        Parent root = sendOrder.load();
+        CurrentOrderDetailController newOrder = sendOrder.getController();
+
+
+
+
     }
 
 
